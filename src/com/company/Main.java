c7b624d3634c2f0dc0def5be5feca499377e6d77
Kiran14700 <kirandhumal89@gmail.com>
2026-03-29 11:03:03 +0530

    Initial project setup with Employee and Main classes

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee(101, "Kiran", 25000);
+        emp1.displayEmployee();
+    }
+}
