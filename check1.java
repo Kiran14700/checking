a1899736c83358a4367e06e4b3ad7285d80d54cc
Kiran14700 <144650256+Kiran14700@users.noreply.github.com>
2026-03-29 10:41:59 +0530

    Add files via upload

---
+++ check1.java
@@ -0,0 +1,8 @@
+public class check1{
+
+public static void main(String[] args){
+
+
+System.out.print("1");
+}
+}
---
+++ check2.java
@@ -0,0 +1,8 @@
+public class check2{
+
+public static void main(String[] args){
+
+
+System.out.print("2");
+}
+}
